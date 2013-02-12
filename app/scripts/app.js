52393f1d4cf5c186173d2ed2573d2d53c0456088
mranosa <mark.ranosa@gmail.com>
2013-02-13 02:29:28 +0800

    Initial site revamp. Needs cleanup soon. User management and goodie goodie still needed

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var trippingIronmanApp = angular.module('trippingIronmanApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+trippingIronmanApp.controller('MainCtrl', [ '$scope', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+}]);
