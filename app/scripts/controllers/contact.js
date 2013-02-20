fa8e390ce07da8c34232448348ea23d772a9e991
mranosa <mark.ranosa@gmail.com>
2013-02-20 19:26:54 +0800

    Initial send email implementation

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,9 +1,19 @@
 'use strict';
 
-trippingIronmanApp.controller('ContactCtrl', ['$scope', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+trippingIronmanApp.controller('ContactCtrl', ['$scope', '$http', function($scope, $http) {
+  
+
+  $scope.sendEmail = function(){
+  	$http.post('https://sendgrid.com/api/mail.send.json', {
+  		api_user: 'markenranosa',
+  		api_key: 'markenranosa',
+  		to: 'mark.ranosa@gmail.com',
+  		from: 'blah@blah.com',
+  		text: 'testingtextbody',
+  		subject: 'email test'
+  	}).success(function(result){
+  		console.log(result);
+  	});
+  }
+
 }]);
